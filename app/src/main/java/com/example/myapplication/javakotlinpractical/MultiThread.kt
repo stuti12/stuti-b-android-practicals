f1e2ec9d5ae968b41871b5d551d0d6cf4af0bd7d
stuti12 <stutibhavsar1@gmail.com>
2022-01-12 14:11:10 +0530

    Practicals of java and kotlin

--- app/src/main/java/com/example/myapplication/javakotlinpractical/MultiThread.kt
+++ app/src/main/java/com/example/myapplication/javakotlinpractical/MultiThread.kt
@@ -18,3 +18,23 @@ fun main(args: Array<String>) {
     val thread1 = Thread(runnable)
     thread1.start() // Will output: Thread[Thread-1,5,main] has run
 }
+
+/*import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    doWorld()
+    println("Done")
+}
+
+// Concurrently executes both sections
+suspend fun doWorld() = coroutineScope { // this: CoroutineScope
+    launch {
+        delay(2000L)
+        println("World 2")
+    }
+    launch {
+        delay(1000L)
+        println("World 1")
+    }
+    println("Hello")
+}*/
